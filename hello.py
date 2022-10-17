79bf1e0256bf6f485e1d0fe7ab4bf6b56a752da4
Paola Rocha <lrocha@cbpi.com.mx>
2022-10-17 14:08:03 -0500

    Chapter 3 Templates

--- hello.py
+++ hello.py
@@ -1,15 +1,16 @@
-from flask import Flask, request, abort
+from flask import Flask, render_template, request, abort
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
     user_agent = request.headers.get('User-Agent')
-    return f'''<h1>Hello World!</h1>
-    <p>Your browser is {user_agent}'''
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, {}!</h1>'.format(name)
+    return render_template('user.html', name=name)
 
 @app.route('/user/<id>')
 def get_user(id):
