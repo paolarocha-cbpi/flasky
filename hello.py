488f9272246c617ecee966c627e0a5b071bcecd7
Paola Rocha <lrocha@cbpi.com.mx>
2022-10-25 13:17:20 -0500

    Chapter 5 databases

--- hello.py
+++ hello.py
@@ -6,16 +6,45 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+# Database configuration
+import os
+from flask_sqlalchemy import SQLAlchemy
 
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'hard to guess string'
+
+app.config['SECRET_KEY'] = 'hard to guess string'  # Necessary for Forms
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
+db = SQLAlchemy(app)
 
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')  # One
+    
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))  # To many
+    
+    def __repr__(self):
+        return '<User %r>' % self.username
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -47,4 +76,4 @@ def page_not_found(e):
 @app.errorhandler(500)  # Unhandled exception
 def internal_server_error(e):
     return render_template('500.html'), 500
-    
+
