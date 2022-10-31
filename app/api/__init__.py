bd0a061267faa1c8969bf98d8529ee2ce512911f
Paola Rocha <lrocha@cbpi.com.mx>
2022-10-31 10:12:33 -0600

    Project configuration

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,3 +1,24 @@
-from flask import Blueprint
-api = Blueprint('api', __name__)
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from config import config
 from . import authentication, posts, users, comments, errors
+from ..main import api as api_blueprint
+
+bootstrap = Bootstrap()
+moment = Moment()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+
+    app.register_blueprint(api_blueprint, url_prefix='/api/v1')
+    bootstrap.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+
+    return app
--- app/api/errors.py
+++ app/api/errors.py
@@ -1,3 +1,6 @@
+from flask import render_template
+from .. import main
+
 @main.app_errorhandler(404)
 def page_not_found(e):
     if request.accept_mimetypes.accept_json and \
